15f5d5371f92d46bc7c66bfb3135c00644479402
atlasmac <mckinleyatlas@gmail.com>
2025-05-08 14:58:26 -0600

    add pic and link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,27 +1,39 @@
-import React from 'react'
-import Image from 'next/image'
-import ContactForm from './ContactForm'
+import React from "react";
+import Image from "next/image";
+import ContactForm from "./ContactForm";
+import Link from "next/link";
 
 function Hero() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
         <Image
-          alt='profile'
+          alt="profile"
           width={500}
           height={500}
           priority
-          src="/prof-pic.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+          src="/atlas.png"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
         <div>
           <h1 className="text-5xl font-bold">Hi, I&apos;m Atlas McKinley</h1>
-          <div className='py-6'>
-            <p className="">I&apos;m a software engineer working for the University of Montana IT department based out of Missoula, Montana.</p>
+          <div className="flex flex-col py-6">
+            <p className="">
+              I&apos;m a software engineer working for the University of Montana
+              IT department based out of Missoula, Montana.
+            </p>
+            <p>
+              Check out my surf forecaster{" "}
+              <a className="link" href="https://magicmoss.app/">
+                Magic Moss 🌊
+              </a>
+            </p>
           </div>
           <ContactForm />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
